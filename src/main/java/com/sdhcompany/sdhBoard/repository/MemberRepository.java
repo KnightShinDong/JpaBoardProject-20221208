552fc2cf0cdbee3d4f6668a72130a8a5e7bef05f
ici <ici@DESKTOP-8CT0OFV>
2022-12-14 17:21:12 +0900

    로그인 기능 구현 중 v0.5

--- src/main/java/com/sdhcompany/sdhBoard/repository/MemberRepository.java
+++ src/main/java/com/sdhcompany/sdhBoard/repository/MemberRepository.java
@@ -1,9 +1,12 @@
 package com.sdhcompany.sdhBoard.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sdhcompany.sdhBoard.entity.SiteMember;
 
 public interface MemberRepository extends JpaRepository<SiteMember, Integer>{
 
+	public  Optional<SiteMember> findByUsername(String username);
 }
